00ce7921f1ba992edfce84ac7a267024f6f6d339
Tsenguun <tsenguun614gmail.com>
2026-06-11 16:41:07 +0800

    fix useWindowSize crash

--- src/useWindowSize.js
+++ src/useWindowSize.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 
 export default function useWindowSize() {
-  const [width, setWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(0);
+  
   useEffect(() => {
+    setWidth(window.innerWidth);
     const handle = () => setWidth(window.innerWidth);
     window.addEventListener("resize", handle);
     return () => window.removeEventListener("resize", handle);
   }, []);
+  
   return width;
 }
